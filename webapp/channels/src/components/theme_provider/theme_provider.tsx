15aaa75a95421b6f4f41f05d2b6f114c2adbc626
Ilya Papou <popov.pilprod@gmail.com>
2026-05-21 23:24:11 +0700

    refactor: remove desktop app theme update notification from ThemeProvider

--- webapp/channels/src/components/theme_provider/theme_provider.tsx
+++ webapp/channels/src/components/theme_provider/theme_provider.tsx
@@ -166,8 +166,6 @@ export default function ThemeProvider({children}: {children: React.ReactNode}) {
 
     useEffect(() => {
         applyTheme(effectiveTheme);
-        // Notify the desktop app so it can update native UI (title bar, etc.).
-        DesktopApp.updateTheme(effectiveTheme);
     }, [effectiveTheme]);
 
 
